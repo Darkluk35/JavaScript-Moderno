9d437f490df78ac59dce39b0e035ecf3b25d73e6
Luis <jesuslara.luis99@gmail.com>
2025-09-06 19:41:20 -0600

    Proyecto terminado y completo con las operaciones en un archivo .js independiente

--- Ejercicios/cotizador-prestamos-react/src/App.jsx
+++ Ejercicios/cotizador-prestamos-react/src/App.jsx
@@ -1,9 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Header from './components/Header'
+import Buttons from './components/Buttons'
+import { formatearDinero, calcularTotalPagar } from './helpers'
 
 function App () {
   const [cantidad, setCantidad] = useState(10000)
+  const [meses, setMeses] = useState(6)
+  const [total, setTotal] = useState(0)
+  const [pago, setPago] = useState(0)
+
+  useEffect(() => {
+    setTotal(calcularTotalPagar(cantidad, meses))
+
+    // Calcular el pago mensual
+  }, [cantidad, meses])
+
+  useEffect(() => {
+    setPago(total / meses)
+  }, [total, meses])
+
   const MIN = 0
   const MAX = 20000
   const STEP = 100
@@ -12,8 +28,24 @@ function App () {
     setCantidad(+e.target.value)
   }
 
+  function handleMeses (e) {
+    setMeses(+e.target.value)
+  }
+
   function handleClickDecremento () {
     const valor = cantidad - STEP
+    if (valor < MIN) {
+      alert('Cantidad no valida')
+      return
+    }
+    setCantidad(valor)
+  }
+  function handleClickIncremento () {
+    const valor = cantidad + STEP
+    if (valor > MAX) {
+      alert('Cantidad no valida')
+      return
+    }
     setCantidad(valor)
   }
 
@@ -23,23 +55,15 @@ function App () {
         <Header />
 
         <div className='flex justify-between my-6'>
-          <button
-            type='button'
-            className='h-10 w-10 flex items-center justify-center font-bold text-white
-            text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2
-          hover:ring-offset-2 hover:ring-lime-500'
-            onClick={handleClickDecremento}
-          >-
-          </button>
-          <button
-            type='button'
-            className='h-10 w-10 flex items-center justify-center font-bold text-white
-            text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2
-          hover:ring-offset-2 hover:ring-lime-500'
-          >+
-          </button>
+          <Buttons
+            operador='-'
+            fn={handleClickDecremento}
+          />
+          <Buttons
+            operador='+'
+            fn={handleClickIncremento}
+          />
         </div>
-
         <input
           className='w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600'
           type='range'
@@ -50,7 +74,30 @@ function App () {
           value={cantidad}
         />
 
-        <p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>{cantidad}</p>
+        <p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>
+          {formatearDinero(cantidad)}
+        </p>
+        <h2 className='text-2xl font-extrabold text-gray-500 text-center'>
+          Elige un <span className='text-indigo-600'>plazo</span> a pagar
+        </h2>
+        <select
+          className='mt-5 w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500'
+          value={meses}
+          onChange={handleMeses}
+        >
+          <option value='6'>6 Meses</option>
+          <option value='12'>12 Meses</option>
+          <option value='24'>24 Meses</option>
+        </select>
+        <div className='my-5 space-y-3 bg-gray-50 p-5'>
+          <h2 className='text-2xl font-extrabold text-gray-500 text-center'>
+            Resumen <span className='text-indigo-600'>de pagos</span>
+            <p className='text-xl text-gray-500 text-center font-bold'>{meses} Meses</p>
+            <p className='text-xl text-gray-500 text-center font-bold'>{formatearDinero(total)} Total a pagar</p>
+            <p className='text-xl text-gray-500 text-center font-bold'>{formatearDinero(pago)} Mensuales</p>
+          </h2>
+        </div>
+
       </div>
     </>
 
