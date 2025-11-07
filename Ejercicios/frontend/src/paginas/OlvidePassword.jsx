cc1931984a64246e8a6ae7eb665eee2711b59bb1
Luis <jesuslara.luis99@gmail.com>
2025-11-06 20:32:12 -0600

    Agregado pages de confirmar y registrar usuario

--- Ejercicios/frontend/src/paginas/OlvidePassword.jsx
+++ Ejercicios/frontend/src/paginas/OlvidePassword.jsx
@@ -1,7 +1,45 @@
+import {Link} from 'react-router-dom'
 const OlvidePassword = () => {
   return (
     <>
-    <h1>Desde OlvidePassword</h1>
+    <div>
+      <h1 className="text-indigo-600 font-black text-5xl">
+        Recupera tu cuenta y administra tus 
+        <span className="text-black"> Pacientes</span>
+        </h1>
+        </div>
+
+        <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
+      <form>
+        <div className="my-5">
+          <label 
+          className=" uppercase text-gray-600 block text-xl font-bold"
+          >Ingresa tu correo</label>
+          <input
+          type="email"
+          placeholder="Email de recuperacion"
+          className=" border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+          />
+        </div>
+        <input 
+            type="submit"
+            value="Recuperar cuenta"
+            className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white 
+            uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800
+            md:w-auto"
+         />
+        </form>
+        <nav className='mt-10 lg:flex lg:justify-between'>
+          <Link 
+          className='block text-center my-5 text-gray-500'
+          to="/">¿Ya tienes una cuenta? Iniciar sesion</Link>
+        <Link 
+          className='block text-center my-5 text-gray-500'
+          to="/registrar">¿No tienes una cuenta? Registrate</Link>
+       
+      </nav>
+        </div>
+      
     </>
   )
 }
